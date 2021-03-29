db23eca1a3dd1480875e3d815817a41a51951198
Giovani Bettoni <bettoni.gn@gmail.com>
2021-03-29 15:49:48 -0300

    Initial Commit

---
+++ MasterChefClass/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MasterChefClass
+//
+//  Created by Giovani Nícolas Bettoni on 29/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MasterChefClass/MasterChefClassApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasterChefClassApp.swift
+//  MasterChefClass
+//
+//  Created by Giovani Nícolas Bettoni on 29/03/21.
+//
+
+import SwiftUI
+
+@main
+struct MasterChefClassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
